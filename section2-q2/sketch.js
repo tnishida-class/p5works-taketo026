a0b8733cc5d6ac07a3600253d2a8c61013e72e08
taketo026 <72774519+taketo026@users.noreply.github.com>
2020-10-26 17:08:23 +0900

    section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,22 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j
+      if((i+j)%2!=0){
+        fill(128);
+      }
+      else{
+        fill(255);
+      }
+      rect(size*i,size*j,size,size);
+      if ((i+j)%2!=0 && j<3){
+        fill(255,0,0);
+        ellipse(size*i+12.5,size*j+12.5,22);
+      }
+      if((i+j)%2!=0 && j>4){
+        fill(0);
+        ellipse(size*i+12.5,size*j+12.5,22);
+      }
     }
   }
 }
