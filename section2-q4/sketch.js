2adb74b3703d9a5ba474d90b2d5970134c745425
taketo026 <72774519+taketo026@users.noreply.github.com>
2020-11-17 12:01:20 +0900

    change 2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -2,12 +2,18 @@
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
+    if(i % 2 != 0){
+      fill(255);
+    }
+    else{
+      fill(blue);
+    }
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     rect(0, i * d, width, (i + 1) * d);
   }
@@ -17,5 +23,8 @@ function setup() {
   rect(0, 0, size, size);
 
   fill(255);
+  rect(2*d,0,d,size);
+  fill(255);
+  rect(0,2*d,size,d);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
